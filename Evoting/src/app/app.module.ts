e70c0d0f2b17252d330c49741582a1e261f92954
Sanjeev Maharjan <sanjeev.maharjan@hotmail.com>
2018-11-20 13:11:22 +0545

    Attempt to apply template (incomplete)

--- Evoting/src/app/app.module.ts
+++ Evoting/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
